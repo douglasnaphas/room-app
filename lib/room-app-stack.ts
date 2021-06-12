b950139527ae2cc2ea9b4446b59290bdb1e34c10
Douglas Naphas <douglasnaphas@gmail.com>
2021-06-12 17:09:05 -0400

    Set up web app props in bin/
    https://github.com/douglasnaphas/room-app/issues/1
    
    This copies some of the code from Brian Liberation needed to do things
    like get configuration params from the AWS account in bin/.

--- lib/room-app-stack.ts
+++ lib/room-app-stack.ts
@@ -1,4 +1,16 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+
+export interface RoomAppProps extends cdk.StackProps {
+  fromAddress?: string;
+  domainName?: string;
+  zoneId?: string;
+  facebookAppId?: string;
+  facebookAppSecret?: string;
+  amazonClientId?: string;
+  amazonClientSecret?: string;
+  googleClientId?: string;
+  googleClientSecret?: string;
+}
 
 export class RoomAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
