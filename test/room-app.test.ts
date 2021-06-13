aa90f395c80ffa7ba4fbb9563490a487bd8358ef
Douglas Naphas <douglasnaphas@gmail.com>
2021-06-12 22:13:34 -0400

    Relax the infrastructure test
    gh-1
    
    I'm really just making sure the stack can be instantiated at this point.
    I haven't found much value in the past in the assertions related to
    testing CDK stacks.

--- test/room-app.test.ts
+++ test/room-app.test.ts
@@ -1,13 +1,15 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as RoomApp from '../lib/room-app-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as RoomApp from "../lib/room-app-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new RoomApp.RoomAppStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new RoomApp.RoomAppStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).notTo(matchTemplate({ Resources: {} }));
 });
